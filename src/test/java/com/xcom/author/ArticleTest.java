f44fbf41e5accf0253cfe243a69c657604133208
RandomErwin <quitcrazy@gmail.com>
2023-04-21 15:07:50 +0800

    3rd

--- src/test/java/com/xcom/author/ArticleTest.java
+++ src/test/java/com/xcom/author/ArticleTest.java
@@ -4,9 +4,11 @@ import com.xcom.author.domain.Article;
 import com.xcom.author.domain.ArticleRepository;
 import com.xcom.author.domain.Comment;
 import com.xcom.author.service.ArticleService;
+import net.minidev.json.JSONArray;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 public class ArticleTest {
@@ -17,8 +19,8 @@ public class ArticleTest {
     @Test
     public void saveArticle(){
         Article article = new Article();
-        article.setTopic("chip war");
-        article.setContent("chip war that combine A with B");
+        article.setTopic("優雅的宇宙");
+        article.setContent("String theory");
         articleService.saveArticle(article);
     }
 
@@ -28,8 +30,20 @@ public class ArticleTest {
         article.setTopic("Artis theory");
         article.setContent("something amazing picture");
         Comment comment1 = new Comment();
-        comment1.setContent("This is an nice book");
+        comment1.setContent("Nice book");
         article.addComment(comment1);
         articleService.updateArticle(article);
+    }
+
+    @Test
+    public void findArticle(){
+        Article article = articleService.findArticle(1L);
+        System.out.println(article);
+    }
+
+    @Transactional
+    @Test
+    public void deleteArticle(){
+        articleService.deleteArticle(52L);
     }
 }
--- src/test/java/com/xcom/author/commentTest.java
+++ src/test/java/com/xcom/author/commentTest.java
@@ -28,4 +28,6 @@ public class commentTest {
 
         commentService.saveComment(comment);
     }
+
+
 }
